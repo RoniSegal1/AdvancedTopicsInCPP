c08b200c9e0e387e90445357561b32a0e2011523
yuvalnamirbarr <128624326+yuvalnamirbarr@users.noreply.github.com>
2025-04-22 13:55:19 +0300

    Add files via upload

---
+++ ex1/Player.cpp
@@ -0,0 +1,23 @@
+#include "Player.h"
+
+Player::Player(int id) : playerId(id) {}
+
+int Player::getId() const {
+    return playerId;
+}
+
+void Player::addTank(Tank* tank) {
+    tanks.insert(tank);
+}
+
+void Player::removeTank(Tank* tank) {
+    tanks.erase(tank);
+}
+
+const std::set<Tank*>& Player::getTanks() const {
+    return tanks;
+}
+
+bool Player::hasAliveTanks() const {
+    return !tanks.empty();
+}
---
+++ ex1/Player.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <set>
+#include "Tank.h"
+
+class Player {
+private:
+    int playerId;
+    std::set<Tank*> tanks; // One tank for now, multiple in future
+
+public:
+    Player(int id);
+
+    int getId() const;
+
+    void addTank(Tank* tank);
+    void removeTank(Tank* tank);
+    const std::set<Tank*>& getTanks() const;
+
+    bool hasAliveTanks() const;
+};
