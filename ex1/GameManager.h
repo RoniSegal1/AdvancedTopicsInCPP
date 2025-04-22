083141963de3e76b95d00f3976de0f83109cef09
yuvalnamirbarr <128624326+yuvalnamirbarr@users.noreply.github.com>
2025-04-22 13:54:42 +0300

    Update GameManager.h

--- ex1/GameManager.h
+++ ex1/GameManager.h
@@ -2,10 +2,17 @@
 #include <string>
 #include "Board.h"
 #include "Tank.h"
+#include "player.h"
 #include "TankAlgorithm.h"
+#include "Entity.h"
+#include <map>
+#include <vector>
+
 
 class GameManager {
 private:
+    std::map<std::pair<int, int>, std::vector<Entity*>> positionMap;
+    // check whether needed:
     Board board;
     Tank tank1;
     Tank tank2;
@@ -21,10 +28,20 @@ public:
     void run();
 
 private:
+    // loadGame() utilities
+    bool readBoardDimensions(std::ifstream& file, int& width, int& height);
+    bool readRawBoardLines(std::ifstream& file, std::vector<std::string>& lines, int width, int height);
+    bool validateRawBoard(const std::vector<std::string>& lines, int width, int height);
+    void updateBoardTerrain(const std::vector<std::string>& lines);
+    void placeTanksFromRawInput(const std::vector<std::string>& lines);
+    //
     void processTurn();
     void applyActions(const std::string& action1, const std::string& action2);
     void moveShells();
+    void rebuildPositionMap();
     void resolveCollisions();
     bool checkWinConditions();
     void logBadAction(int playerId, const std::string& action);
+
+    void destroyTank(Tank* tank, Player* owner);
 };
