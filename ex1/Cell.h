995e8d2691b8da43ca1c898c54f306d2c1fe7ce7
yuvalnamirbarr <128624326+yuvalnamirbarr@users.noreply.github.com>
2025-04-22 13:53:19 +0300

    Update Cell.h

--- ex1/Cell.h
+++ ex1/Cell.h
@@ -17,8 +17,6 @@ private:
     int y;
     TerrainType terrain;
     int wallHits; // -1 if no wall
-    std::set<Tank*> tanks;
-    std::set<Shell*> shells; // number of shells in Cell
 
 public:
     Cell(int x, int y);
@@ -31,14 +29,4 @@ public:
     int getWallHits() const;
     void incrementWallHits();
     void resetWall();
-
-    void addTank(Tank* tank);
-    void removeTank(Tank* tank);
-    const std::set<Tank*>& getTanks() const;
-    void clearTanks();
-
-    void addShell(Shell* shell);
-    void removeShell(Shell* shell);
-    const std::set<Shell*> getShells() const;
-    void clearShells();
 };
