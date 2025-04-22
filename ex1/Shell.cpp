0796bb9d284f4b85a8a828b7b36f367843e6cc38
yuvalnamirbarr <128624326+yuvalnamirbarr@users.noreply.github.com>
2025-04-22 13:55:59 +0300

    Update Shell.cpp

--- ex1/Shell.cpp
+++ ex1/Shell.cpp
@@ -1,18 +1,35 @@
 #include "Shell.h"
+#include <utility>
 
-int Shell::nextSerialNumber = 1;
-
-Shell::Shell(Direction dir)
-    : direction(dir), serialNumber(nextSerialNumber++) {}
+Shell::Shell(int x, int y, Direction dir)
+    : x(x), y(y), direction(dir) {}
 
 Direction Shell::getDirection() const {
     return direction;
 }
 
-void Shell::setDirection(Direction newDir) {
-    direction = newDir;
+std::pair<int, int> Shell::getPosition() const {
+    return {x,y};
 }
 
-int Shell::getSerialNumber() const {
-    return serialNumber;
+void Shell::setPosition(int newX, int newY) {
+    x = newX;
+    y = newY;
+}
+
+std::pair<int, int> Shell::getNextShellPosition() const {
+    int dx = 0, dy = 0;
+
+    switch (direction) {
+        case Direction::U:  dy = -2; break;
+        case Direction::UR: dy = -2; dx = +2; break;
+        case Direction::R:  dx = +2; break;
+        case Direction::DR: dy = +2; dx = +2; break;
+        case Direction::D:  dy = +2; break;
+        case Direction::DL: dy = +2; dx = -2; break;
+        case Direction::L:  dx = -2; break;
+        case Direction::UL: dy = -2; dx = -2; break;
+    }
+
+    return {x + dx, y + dy};
 }
