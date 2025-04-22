d094d4b728f85d30d7658e14c52f3cc53f6ebcd9
yuvalnamirbarr <128624326+yuvalnamirbarr@users.noreply.github.com>
2025-04-22 13:56:54 +0300

    Update Tank.cpp

--- ex1/Tank.cpp
+++ ex1/Tank.cpp
@@ -1,9 +1,7 @@
 #include "Tank.h"
 
-int Tank::nextSerialNumber = 1;
-
-Tank::Tank(Direction dir, int ammo, int playerId)
-    : direction(dir), ammo(ammo), playerId(playerId), serialNumber(nextSerialNumber++) {}
+Tank::Tank(Direction dir, int x, int y, int player, int ammo)
+    : direction(dir), ammo(ammo), player(player), x(x), y(y) {}
 
 Tank::~Tank() {}
 
@@ -15,19 +13,51 @@ void Tank::setDirection(Direction dir) {
     direction = dir;
 }
 
+std::pair<int, int> Tank::getPosition() const {
+    return {x, y};
+}
+
 int Tank::getAmmo() const {
     return ammo;
 }
 
-int Tank::getSerialNumber() const {
-    return serialNumber;
+int Tank::getPlayer() const {
+    return player;
+}
+
+void Tank::setPosition(int newX, int newY) {
+    x = newX;
+    y = newY;
+}
+
+std::pair<int, int> Tank::getNextForwardPosition() const {
+    int dx = 0, dy = 0;
+
+    switch (direction) {
+        case Direction::U:  dy = -1; break;
+        case Direction::UR: dy = -1; dx = +1; break;
+        case Direction::R:  dx = +1; break;
+        case Direction::DR: dy = +1; dx = +1; break;
+        case Direction::D:  dy = +1; break;
+        case Direction::DL: dy = +1; dx = -1; break;
+        case Direction::L:  dx = -1; break;
+        case Direction::UL: dy = -1; dx = -1; break;
+    }
+
+    return {x + dx, y + dy};
+}
+
+void Tank::rotate(int place){
+    int dirValue = (static_cast<int>(direction) + place) % 8;
+    if (dirValue < 0) dirValue += 8;
+    setDirection(static_cast<Direction>(dirValue));
 }
 
 Shell* Tank::shoot() {
     if (canShoot()) {
         ammo--;
         startShootCooldown();
-        return new Shell(direction); // Shell gets its own serial number internally
+        return new Shell(x, y, direction); // Shell gets its own serial number internally
     }
     return nullptr;
 }
@@ -45,8 +75,8 @@ void Tank::startBackwardDelay() {
         backwardDelay = 2;
 }
 
-bool Tank::isReadyToMoveBackward() const {
-    return backwardDelay == 1;
+bool Tank::notInBackwardDelay() const {
+    return backwardDelay == 0;
 }
 
 void Tank::tick() {
