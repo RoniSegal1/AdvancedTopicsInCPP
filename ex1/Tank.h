c137f8c8d8135a637926427a9150f1261eb0fdf0
yuvalnamirbarr <128624326+yuvalnamirbarr@users.noreply.github.com>
2025-04-22 13:57:10 +0300

    Update Tank.h

--- ex1/Tank.h
+++ ex1/Tank.h
@@ -1,25 +1,34 @@
 #pragma once
 #include "Direction.h"
 #include "Shell.h"
+#include "Entity.h"
+#include <set>
+#include <utility>
 
-class Tank {
+class Tank : public Entity {
 private:
     Direction direction;
+    int x;
+    int y;
     int ammo;
-    int serialNumber;
-    int playerId;
+    int player;
 
     int shootCooldown = 0;  // counts down after each shot
     int backwardDelay = 0;  // 0 = not waiting, >0 = waiting for backward move
 
-    static int nextSerialNumber;
-
 public:
-    Tank(Direction dir, int ammo = 16);
-    ~Tank();
+    Tank(Direction dir, int x, int y, int player, int ammo = 16);
 
     Direction getDirection() const;
     void setDirection(Direction dir);
+
+    std::pair<int, int> getPosition() const override;
+    void setPosition(int newX, int newY);
+
+    std::pair<int, int> Tank::getNextForwardPosition() const;
+    void rotate(int place);
+
+    int Tank::getPlayer() const;
 
     int getAmmo() const;
     int getSerialNumber() const;
@@ -30,7 +39,7 @@ public:
     bool canShoot() const;                // check if shooting is allowed
 
     void startBackwardDelay();            // begin delay for backward movement
-    bool isReadyToMoveBackward() const;   // ready to move on next tick
+    bool notInBackwardDelay() const;      // ready to move on next tick
 
     void tick();                          // update cooldowns and delays per game step
 };
